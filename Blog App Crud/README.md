a427de7ee95570705424331da9e81a24e50cd774
Raghavendran Natarajan <raghavendran.natarajan@kyndryl.com>
2022-02-26 18:48:29 +0530

    New commit

--- Blog App Crud/README.md
+++ Blog App Crud/README.md
@@ -1,6 +1,6 @@
-# MyApp
+# Firebase9
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.3.
 
 ## Development server
 
--- Blog App Crud/src/app/app.module.ts
+++ Blog App Crud/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirebaseCrudComponent } from './firebase-crud/firebase-crud.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirebaseCrudComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
